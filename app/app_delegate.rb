419422a16bc39b4daff977b008bc270f259ef5cc
cactis <cactis.lin@gmail.com>
2014-08-06 15:56:50 +0800

    remove idenfifier

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,7 +4,6 @@ class AppDelegate
   UUID = NSUUID.alloc.initWithUUIDString("B91A2F40-2725-4926-8E3B-AB18425ACC11")
 
   def applicationDidFinishLaunching(notification)
-    ap __method__
     @app_name = NSBundle.mainBundle.infoDictionary['CFBundleDisplayName']
 
     @status_menu = NSMenu.new
@@ -22,31 +21,25 @@ class AppDelegate
   end
 
   def createMenuItem(name, action)
-    ap __method__
     NSMenuItem.alloc.initWithTitle(name, action: action, keyEquivalent: '')
   end
 
   def iBeaconStart
-    ap __method__
-    identifier = "tw.edu.sce.pccu.ibeacon" # "com.willrax.region"
     @beaconRegion = BLCBeaconAdvertisementData.alloc.initWithProximityUUID UUID, major: 10, minor: 0, measuredPower: -58
     @peripheralManager = CBPeripheralManager.alloc.initWithDelegate(self, queue: nil)
   end
 
   def iBeaconStop
-    ap __method__
     @peripheralManager.stopAdvertising
   end
 
   def peripheralManagerDidUpdateState(peripheral)
-    ap __method__
     if peripheral.state == CBPeripheralManagerStatePoweredOn
       @peripheralManager.startAdvertising @beaconRegion.beaconAdvertisement
     end
   end
 
   def pressAction
-    ap __method__
     alert = NSAlert.alloc.init
     alert.setMessageText "Action triggered from status bar menu"
     alert.addButtonWithTitle "OK"
